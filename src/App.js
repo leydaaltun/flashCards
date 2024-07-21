f2372f86c0238175cfafc9811139df7fec5f138e
leydaaltun <altunleyda@gmail.com>
2024-07-21 10:02:32 +0300

    edited(app .js)

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function FlashCards() {
   console.log("test", selectedId);
 
   function handleClick(id) {
-    setSelectedId(id)
+    setSelectedId(id !== selectedId ? id : null);
   }
   return (
     <div className="flashcards">
